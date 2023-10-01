a0fdd869396b6ee18af52515234c0d62acdf567e
soundaryaNaik <sonu.organizein@gmail.com>
2023-10-01 16:32:09 +0530

    Fixed Some Issues

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "4b353eec9a0f5405e39ba36498da85f9",
   base: "https://api.openweathermap.org/data/2.5/",
 };
